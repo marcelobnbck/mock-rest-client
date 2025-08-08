53f2998dd900a9a23deed8878347fdb8bcf3b2cd
Marcelo Beck <marcelobck@gmail.com>
2025-08-07 22:49:13 -0300

    Pom dependencies adjusts fix imports

--- src/test/java/com/example/graphql/client/UserClientTest.java
+++ src/test/java/com/example/graphql/client/UserClientTest.java
@@ -1,45 +1,24 @@
 package com.example.graphql.client;
 
 import com.example.graphql.model.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-@TestConfiguration
-class RestTemplateConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
-
-    @RestClientTest(UserClient.class)
+@RestClientTest(UserClient.class)
 class UserClientTest {
 
     @Autowired
     private UserClient userClient;
 
     @Autowired
-    private RestTemplate restTemplate;
-
     private MockRestServiceServer mockServer;
-
-    @BeforeEach
-    void setup() {
-        mockServer = MockRestServiceServer.createServer(restTemplate);
-    }
 
     @Test
     void shouldFetchUserSuccessfully() {
